bfceb183ab06bd1699c51177b87e0649be5cfab2
tim-z-donut <200664920+tim-z-donut@users.noreply.github.com>
2025-10-19 18:53:01 -0400

    Initial Commit: Pygame window opens and closes

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+def main():
+    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
