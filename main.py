7d74e347848b43fdbed0f547499a703481dafa91
tim-z-donut <200664920+tim-z-donut@users.noreply.github.com>
2025-10-21 18:30:49 -0400

    Player is drawn and movable using wasd, objects are grouped together under updatable and drawable to simplify main.py's logic

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
 
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    # print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,7 +23,14 @@ def main():
                 return
         screen.fill("black")
 
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
+        # print(dt)
 
 if __name__ == "__main__":
     main()
